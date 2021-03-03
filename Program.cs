d1f621567e64384936519e0b8f6e76cbed83a225
ALEX ALEXANDRE ALVES <alex.alves@alunos.sc.senac.br>
2021-03-02 21:06:50 -0300

    Update 02.03.2021

--- Program.cs
+++ Program.cs
@@ -1,43 +1,49 @@
 using System;
-using View;
-using Models;
-using Controllers;
 
-namespace MvcExample
+namespace csharp_vehicles
 {
     public class Program
     {
-        public static void Main()
+        /// <summary>
+        /// Start the system
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
         {
-            int menu = 0;
-            do
-            {
-                Console.WriteLine("Digite a opção do programa:");
-                Console.WriteLine("1 - Inserir Cliente:");
-                Console.WriteLine("2 - Listar Cliente:");
-
+            int opt = 0;
+            Console.WriteLine("Bem vindo a nossa revenda");
+            // Always repeat until the user leaves
+            do {
+                Console.WriteLine("Digite a operação de Menu");
+                Console.WriteLine("1 - Cadastrar Cliente");
+                Console.WriteLine("2 - Lista de Clientes");
+                Console.WriteLine("3 - Cadastrar Veículo Pesado");
+                Console.WriteLine("4 - Lista de Veículos Pesados");
                 Console.WriteLine("0 - Sair");
-
-                menu = Convert.ToInt32(Console.ReadLine());
-                switch (menu)
-                {
+                // Get the user option
+                opt = Convert.ToInt32(Console.ReadLine());
+                switch(opt) {
                     case 0:
+                        // Close system
                         break;
                     case 1:
-                        InserirCliente();
+                        View.Customer.CreateCustomer();
                         break;
                     case 2:
-                        ListarClientes();
+                        View.Customer.ListCustomers();
                         break;
-
+                    case 3:
+                        View.HeavyVehicle.CreateVehicle();
+                        break;
+                    case 4:
+                        View.HeavyVehicle.ListVehicles();
+                        break;
                     default:
-                        Console.WriteLine("Opção inválida");
+                        Console.WriteLine("Operação Inválida.");
                         break;
                 }
-            } while (menu != 0);
-     
-     
-     
+            } while(opt != 0);
         }
+
     }
 }
