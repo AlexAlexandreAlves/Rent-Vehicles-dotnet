20f43a7fce6e85461a6525c871f358720e6643c8
ALEX ALEXANDRE ALVES <alex.alves@alunos.sc.senac.br>
2021-03-23 21:56:45 -0300

    Db_Changes 23.03.2021

---
+++ Controller/LightVehicle.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+namespace Controller
+{
+    public class LightVehicle
+    {
+        public static void CreateLightVehicle(
+            string Brand,
+            string Model,
+            string Year,
+            string Price,
+            string Color
+        )
+        {
+            int ConvertYear = Convert.ToInt32(Year);
+            double ConvertPrice = Convert.ToDouble(Price);
+
+            if (ConvertYear < 1990) {
+                throw new Exception("Carro muito antigo");  
+            }
+
+            if (ConvertPrice < 0) {
+                throw new Exception("Valor não pode ser negativo");
+            }
+
+            new Model.LightVehicle(
+                Brand,
+                Model,
+                ConvertYear,
+                ConvertPrice,
+                Color
+            );
+        }
+
+        public static List<Model.LightVehicle> GetLightVehicles() {
+            return Model.LightVehicle.GetLightVehicles();
+        }
+    }
+}
