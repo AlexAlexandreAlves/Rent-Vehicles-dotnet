0ee5403c7ab05bb9434b000678704b23f8198ba8
ALEX ALEXANDRE ALVES <alex.alves@alunos.sc.senac.br>
2021-06-22 22:09:31 -0300

    Update connect db cloud
    Signed-off-by: ALEX ALEXANDRE ALVES <alex.alves@alunos.sc.senac.br>

--- Repository/Repositorio.cs
+++ Repository/Repositorio.cs
@@ -5,14 +5,33 @@ namespace Repository
 {
     public class Context : DbContext
     {
-        public DbSet<Cliente> Clientes {get; set;}
-        public DbSet<VeiculoPesado> VeiculosPesados  {get; set;}
-        public DbSet<VeiculoLeve> VeiculosLeves  {get; set;}
-        public DbSet<Locacao> Locacoes {get; set;}
-        public DbSet<LocacaoVeiculoPesado> LocacaoVeiculoPesados {get; set;}
-        public DbSet<LocacaoVeiculoLeve> LocacaoVeiculoLeves {get; set;}
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<VeiculoPesado> VeiculosPesados { get; set; }
+        public DbSet<VeiculoLeve> VeiculosLeves { get; set; }
+        public DbSet<Locacao> Locacoes { get; set; }
+        public DbSet<LocacaoVeiculoPesado> LocacaoVeiculoPesados { get; set; }
+        public DbSet<LocacaoVeiculoLeve> LocacaoVeiculoLeves { get; set; }
 
-         protected override void OnConfiguring(DbContextOptionsBuilder options)
-            => options.UseMySql("Server=localhost;User Id=root;Database=LocacoesVeiculos");
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+
+            string connStr;
+
+            string connUser = "bf07458e8e5095";
+            string connPass = "236c2802";
+            string connHost = "us-cdbr-east-04.cleardb.com";
+            string connDb = "heroku_1fb931cf91c4b37";
+
+            string connStr = $"server={connHost};Uid={connUser};Pwd={connPass};Database={connDb};SSL Mode=None";
+
+            options.UseMySql(connStr, mySqlOptionsAction: mysqlOptions =>
+            {
+                mysqlOptions.EnableRetryOnFailure(
+                maxRetryCount: 1,
+                maxRetryDelay: TimeSpan.FromSeconds(30),
+                errorNumbersToAdd: null
+        );
+            });
+        }
     }
 }
