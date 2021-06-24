abc635e07a9e68b42519d5d4a0d3e584478c84c5
ALEX ALEXANDRE ALVES <alex.alves@alunos.sc.senac.br>
2021-06-24 20:51:38 -0300

    Update cloud base
    Signed-off-by: ALEX ALEXANDRE ALVES <alex.alves@alunos.sc.senac.br>

--- Repository/Repositorio.cs
+++ Repository/Repositorio.cs
@@ -12,26 +12,75 @@ namespace Repository
         public DbSet<LocacaoVeiculoPesado> LocacaoVeiculoPesados { get; set; }
         public DbSet<LocacaoVeiculoLeve> LocacaoVeiculoLeves { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        //protected override void OnConfiguring(DbContextOptionsBuilder options)
+        // {
+
+        //string connStr;
+
+        // string connPass = "236c2802";
+        //string connHost = "us-cdbr-east-04.cleardb.com";
+        //string connDb = "heroku_1fb931cf91c4b37";
+
+        //string connStr = $"server={connHost};Uid={connUser};Pwd={connPass};Database={connDb};SSL Mode=None";
+
+        // options.UseMySql(connStr, mySqlOptionsAction: mysqlOptions =>
+        ////  {
+        //   (
+        //     maxRetryCount: 1,
+        //    maxRetryDelay: TimeSpan.FromSeconds(30),
+        //  errorNumbersToAdd: null
+        // );
+        //  });
+        //}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+
         {
 
-            string connStr;
-
             string connUser = "bf07458e8e5095";
+
             string connPass = "236c2802";
+
             string connHost = "us-cdbr-east-04.cleardb.com";
+
             string connDb = "heroku_1fb931cf91c4b37";
+
+
 
             string connStr = $"server={connHost};Uid={connUser};Pwd={connPass};Database={connDb};SSL Mode=None";
 
-            options.UseMySql(connStr, mySqlOptionsAction: mysqlOptions =>
+
+
+            // optionsBuilder.UseMySql(
+
+            //     "server=localhost;user=root;password=;database=locacar;",
+
+            //     new MySqlServerVersion("8.0.22")
+
+            // );
+
+
+
+            optionsBuilder.UseMySql(
+
+            connStr, new MySqlServerVersion("8.0.22"),
+
+            mySqlOptionsAction: mysqlOptions =>
+
             {
+
                 mysqlOptions.EnableRetryOnFailure(
-                maxRetryCount: 1,
-                maxRetryDelay: TimeSpan.FromSeconds(30),
-                errorNumbersToAdd: null
-        );
+
+                    maxRetryCount: 1,
+
+                    maxRetryDelay: TimeSpan.FromSeconds(30),
+
+                    errorNumbersToAdd: null
+
+                );
+
             });
+
         }
     }
 }
